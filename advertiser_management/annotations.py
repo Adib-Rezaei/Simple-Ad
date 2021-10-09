a4b308e174898f84ae881f5d80fa9b72cf7204ec
Adib-Rezaei <adibrezaeish@gmail.com>
2021-10-09 18:07:39 +0330

    refac: minor changes

--- advertiser_management/annotations.py
+++ advertiser_management/annotations.py
@@ -1,12 +1,8 @@
-from django.db.models.aggregates import Avg, Count, Sum
-from django.db.models.expressions import F, OuterRef, Subquery
+from django.db.models.aggregates import Avg, Count
 from django.db.models.fields import FloatField
-from django.db.models.functions import Coalesce
 from django.db.models.functions.comparison import Cast
 from django.db.models.query_utils import Q
-from django.utils import timezone
 
-from advertiser_management.models.event import Click, View
 
 
 def annotate_hourly_ad_events(queryset, hour):
@@ -44,5 +40,3 @@ def annotate_ads_report(queryset, hour):
                 annotate_hourly_ad_events(queryset, hour)
             )
         )
-
-# Click.objects.filter(ad__id=OuterRef('id'), created__time__hour__range=[hour, hour+1])
